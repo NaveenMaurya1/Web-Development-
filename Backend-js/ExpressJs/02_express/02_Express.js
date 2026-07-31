71f0f6c1fa95e1050c14e7e001eaf53d52c9867b
Naveen_M <naveenmauryagng@gmail.com>
2026-07-31 18:16:01 +0530

    removed unwanted files

---
+++ Backend-js/ExpressJs/02_express/02_Express.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {                 // this is known as chaining of requests
+  console.log("hello get request")
+  res.send('Hello World! Get')
+}).post('/', (req, res) => {
+  console.log("hello Post request ")
+  res.send('Hello World! Post ')
+}).put('/', (req, res) => {
+  console.log("hello PUT request ")
+  res.send('Hello World! PUT ')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
