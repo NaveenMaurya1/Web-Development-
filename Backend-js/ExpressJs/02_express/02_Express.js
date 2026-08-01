df1f26af636aedd363242ad226230b5bd362aa68
Naveen_M <naveenmauryagng@gmail.com>
2026-08-01 21:03:59 +0530

    Request of chaining

--- Backend-js/ExpressJs/02_express/02_Express.js
+++ Backend-js/ExpressJs/02_express/02_Express.js
@@ -15,6 +15,20 @@ app.get('/', (req, res) => {                 // this is known as chaining of req
   res.send('Hello World! PUT ')
 })
 
+
+// app.get('/', (req, res) => {               
+//   console.log("hello get request")
+//   res.send('Hello World! Get')
+// })
+// app.post('/', (req, res) => {
+//   console.log("hello Post request ")
+//   res.send('Hello World! Post ')
+// })
+// app.put('/', (req, res) => {
+//   console.log("hello PUT request ")
+//   res.send('Hello World! PUT ')
+// })
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 })
