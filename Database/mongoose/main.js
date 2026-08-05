e898fbb9bff4937054ef07e740b7ce45928304f3
Naveen_M <naveenmauryagng@gmail.com>
2026-08-05 11:30:20 +0530

    connecting databse correctly

--- Database/mongoose/main.js
+++ Database/mongoose/main.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
-import {Todo} from "./models/Todo";
+import { Todo } from "./models/Todo.js";
 
-let a = await mongoose.connect("mongodb://localhost:27017/")
+await mongoose.connect("mongodb://127.0.0.1:27017/TodoDB");
 
 import express from 'express';
 
@@ -10,6 +10,28 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
+
+  const todo = new Todo({ title: "Getting Started with Node.js",
+  author: "Praveen Maurya",
+  body: "Node.js is a JavaScript runtime built on Chrome's V8 engine. It allows developers to build fast and scalable server-side applications using JavaScript.",
+  comments: [
+    {
+      body: "Great article! Very easy to understand.",
+      date: new Date("2026-08-05")
+    },
+    {
+      body: "Can you make one on Express.js?",
+      date: new Date("2026-08-06")
+    }
+  ],
+  date: new Date(),
+  hidden: false,
+  meta: {
+    votes: 156,
+    favs: 87
+  }})
+  todo.save()
+
   res.send('Hello World!');
 });
 
