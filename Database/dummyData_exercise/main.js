ca3a2d4752b6c962a152a4b7645be8038b96a735
Naveen_M <naveenmauryagng@gmail.com>
2026-08-06 16:37:44 +0530

    random data generation activated

--- Database/dummyData_exercise/main.js
+++ Database/dummyData_exercise/main.js
@@ -2,10 +2,46 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose');
+const Employee = require('./models/Employee');
+
+(async () => {
+  await mongoose.connect('mongodb://localhost:27017/company');
+  console.log('MongoDB connected');
+})();
+
+app.set('view engine','ejs')
+
+const getRandom = (arr) =>{
+    let random = Math.floor(Math.random() *( arr.length -1)) ;
+    return arr[random];
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index',{foo:'FOO'})
+})
+app.get('/Genrate', async (req, res) => {
+// Genrate random Data 
+   let randomName = ["Rohan","Sohan","Shyam","Deepak","Harry"];
+   let randomSalary = [10000,20000,30000,40000,50000];
+   let randomLang = ["Python","C++","C#","Java","JS"];
+   let randomCity = ["New york","Quabec","California","Banglore","Seattle"];
+   let randomManager = ["true","false"];
+
+   for (let i = 0; i < 10; i++) {
+   let e = await Employee.create({
+      name: getRandom(randomName), 
+      salary: getRandom(randomSalary), 
+      language: getRandom(randomLang), 
+      city: getRandom(randomCity), 
+      isManager: getRandom(randomManager)
+   })
+    console.log(e)
+   }
+
+  res.render('index',{foo:'FOO'})
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
