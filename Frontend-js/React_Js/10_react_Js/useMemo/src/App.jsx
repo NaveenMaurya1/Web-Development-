e42c44ebd57663ac2f61fbafb7bacbd55d2352b6
Naveen_M <naveenmauryagng@gmail.com>
2026-08-13 16:28:20 +0530

    UseMemo Hook in react

---
+++ Frontend-js/React_Js/10_react_Js/useMemo/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState, useMemo } from 'react'
+
+
+import './App.css'
+
+
+const nums = new Array(30_000).fill(0).map((_, i)=>{
+  return {
+    index: i,
+    isMagical: i===29_000
+  }
+})
+
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [numbers, setNumbers] = useState(nums)
+
+  // const magical = numbers.find(item=>item.isMagical===true) // Expensive Computation that's why it can slow down the processing.
+
+  const magical = useMemo(() => numbers.find(item=>item.isMagical===true), [numbers]) 
+
+  return (
+    <>
+      <div>
+
+        <span>Magical number is {magical.index}</span>
+
+     
+      </div>
+      <h1>Vite + React</h1>
+
+      <div className="card">
+
+        <button
+         onClick={() => {
+          setCount((count) => count + 1);
+          if(count == 10){
+            setNumbers(new Array(10_000).fill(0).map((_, i)=>{
+              return {
+                index: i,
+                isMagical: i===9_000
+              }
+            }))
+          }
+        
+        }}
+
+        >
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
